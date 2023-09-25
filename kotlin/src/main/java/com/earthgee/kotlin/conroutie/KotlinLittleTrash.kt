e93f651a7e79144f46017787d5b02497d1c3a64b
zhaoruixuan <zhaoruixuan@haodf.com>
2023-09-25 10:55:03 +0800

    coroutine test

---
+++ kotlin/src/main/java/com/earthgee/kotlin/conroutie/KotlinLittleTrash.kt
@@ -0,0 +1,36 @@
+package com.earthgee.kotlin.conroutie
+
+import kotlinx.coroutines.*
+
+/**
+ *  Created by zhaoruixuan1 on 2023/9/25
+ *  CopyRight (c) haodf.com
+ *  功能：
+ */
+class KotlinLittleTrash {
+
+    fun main() = testAsync()
+
+    private fun testJobJoin() = runBlocking {
+        val job = GlobalScope.launch(Dispatchers.Main) {
+            delay(1000L)
+            println("World,${Thread.currentThread().name}")
+        }
+        println("Hello,${Thread.currentThread().name}")
+        job.join()
+    }
+
+    private fun testAsync() = runBlocking {
+        val deferred = async {
+            loadData()
+        }
+        val result = deferred.await()
+    }
+
+    private suspend fun loadData(): Int {
+        delay(1000L)
+        println("loaded!${Thread.currentThread().name}")
+        return 42
+    }
+
+}
