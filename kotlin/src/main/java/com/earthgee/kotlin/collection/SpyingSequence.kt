82293bbeeaebd5fb8f031cf7511a4c35400160e4
zhaoruixuan <zhaoruixuan@haodf.com>
2023-09-25 09:44:02 +0800

    sequence test

---
+++ kotlin/src/main/java/com/earthgee/kotlin/collection/SpyingSequence.kt
@@ -0,0 +1,27 @@
+package com.earthgee.kotlin.collection
+
+import android.util.Log
+
+/**
+ *  Created by zhaoruixuan1 on 2023/9/22
+ *  CopyRight (c) haodf.com
+ *  功能：
+ */
+class SpyingSequence<T>(private val underlyingSequence: Sequence<T>) : Sequence<T>{
+
+    override fun iterator() = object : Iterator<T> {
+        val underlyingIterator = underlyingSequence.iterator()
+
+        override fun hasNext(): Boolean = underlyingIterator.hasNext()
+
+        override fun next(): T {
+            val item = underlyingIterator.next()
+            Log.d("earthgee", "spy: $item")
+            return item
+        }
+
+    }
+
+}
+
+fun <T> Sequence<T>.spy() = SpyingSequence(this)
