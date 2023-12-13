70c9c780b562577890d2706d95d38c69ed6c0a54
zhaoruixuan <zhaoruixuan@haodf.com>
2023-12-13 16:01:03 +0800

    jetpack test2

--- kotlin/src/main/java/com/earthgee/kotlin/conroutie/FlowTest.kt
+++ kotlin/src/main/java/com/earthgee/kotlin/conroutie/FlowTest.kt
@@ -1,11 +1,9 @@
 package com.earthgee.kotlin.conroutie
 
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 /**
  *  Created by zhaoruixuan1 on 2023/9/27
@@ -19,12 +17,33 @@ fun foo(): Flow<Int> = flow {
     }
 }
 
-fun main() = runBlocking {
-    launch {
-        (1..3).forEach { 
-            println("not block $it")
-            delay(1000)
+//fun main() = runBlocking {
+//    launch {
+//        (1..3).forEach {
+//            println("not block $it")
+//            delay(1000)
+//        }
+//    }
+//    foo().collect { value -> print(value) }
+//}
+
+fun testCancel() = runBlocking {
+    val cancelJob = launch {
+        repeat(1000) {
+            if(!isActive) {
+                return@repeat
+            }
+
+            println("running")
         }
     }
-    foo().collect { value -> print(value) }
+
+    delay(1)
+    println("Cancel")
+    cancelJob.cancel()
+    println("Done")
+}
+
+suspend fun testCatchException() = CoroutineScope() {
+
 }
